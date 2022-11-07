9bf53f7eb95c0256b5114a2b6eb966a064d0b9e2
DrC0okie <timothee.vanhove@heig-vd.ch>
2022-11-07 10:09:09 +0100

    Begins to work

--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
+++ ServerMultiThread/src/main/java/ch/heigvd/api/calc/ServerWorker.java
@@ -7,7 +7,9 @@ import java.util.Stack;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/** Calculator worker implementation */
+/**
+ * Calculator worker implementation
+ */
 public class ServerWorker implements Runnable {
 
     private final static Logger LOG = Logger.getLogger(ServerWorker.class.getName());
@@ -15,15 +17,20 @@ public class ServerWorker implements Runnable {
     private BufferedReader in = null;
     private BufferedWriter out = null;
 
-    /** Instantiation of a new worker mapped to a socket
-     * @param clientSocket connected to worker */
+    /**
+     * Instantiation of a new worker mapped to a socket
+     *
+     * @param clientSocket connected to worker
+     */
     public ServerWorker(Socket clientSocket) {
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
         this.clientSocket = clientSocket;
     }
 
-    /** Run method of the thread. */
+    /**
+     * Run method of the thread.
+     */
     @Override
     public void run() {
         while (!clientSocket.isClosed()) {
@@ -33,20 +40,24 @@ public class ServerWorker implements Runnable {
                 String msg = in.readLine();
                 System.out.println(msg);
 
-                if (msg.equals("CLOSE")) {
-                    out.write("Closing connexion. Goodbye Jarod!\r\n");
+                if (msg.startsWith("CLOSE")) {
+                    out.write("Closing connexion. Goodbye Jarod!\n");
                     out.flush();
                     clientSocket.close();
                     in.close();
                     out.close();
-                }
-                else{
-                    Double result = processOperation(msg);
-                    if(result.isNaN())
-                        out.write("Invalid expression : " + msg);
-                    else
-                        out.write(result.toString());
-
+                } else if(msg.startsWith("COMPUTE")) {
+                    msg = msg.substring(7);
+                    try{
+                        Double result = processOperation(msg);
+                        if (result.isNaN())
+                            out.write("ERROR SYNTAX\n");
+                        else
+                            out.write("OK RESULT " + result + "\n");
+                    }
+                    catch(RuntimeException e){
+                        out.write(e.getMessage());
+                    }
                     out.flush();
                 }
 
@@ -56,27 +67,29 @@ public class ServerWorker implements Runnable {
         }
     }
 
-    private Double processOperation(String msg){
+    private Double processOperation(String msg)throws RuntimeException {
         Stack<Double> valStack = new Stack<>();
         Stack<Character> opStack = new Stack<>();
-        StringBuilder s = new StringBuilder();
-        for(int i = 0; i < msg.length(); ++i){
+
+        for (int i = 0; i < msg.length(); ++i) {
             char c = msg.charAt(i);
-            if(c == ' ' || c == '('){
+            if (c == ' ' || c == '(') {
                 //Do nothing
-            }
-            else if(Character.isDigit(c)){
-                 for(int j = 0; Character.isDigit(msg.charAt(i + j)); j++){
-                     s.append(msg.charAt(i + j));
-                     if(i + j == msg.length() - 1)
-                         break;
-                 }
+            } else if (Character.isDigit(c)) {
+                // Concat all next digital char to compose a number
+                int nextCharIndex = 0;
+                StringBuilder s = new StringBuilder();
+                while (Character.isDigit(msg.charAt(i + nextCharIndex))) {
+                    s.append(msg.charAt(i + nextCharIndex));
+                    nextCharIndex++;
+                }
+                //Increment the main index
+                i += nextCharIndex - 1;
+                // Push the number in the value stack
                 valStack.push(Double.parseDouble(s.toString()));
-                if(i < msg.length() - 1)
-                    i += s.length() - 1;
+                //Delete the temporary formed string
                 s.delete(0, s.length());
-            }
-            else if(c == ')'){
+            } else if (c == ')') {
                 double v = valStack.pop();
                 char op = opStack.pop();
                 if (op == '+') {
@@ -86,14 +99,14 @@ public class ServerWorker implements Runnable {
                 } else if (op == '*') {
                     v = valStack.pop() * v;
                 } else if (op == '/') {
+                    if(v == 0)
+                        throw new RuntimeException("ERROR DEFINITION_DOMAIN\n");
                     v = valStack.pop() / v;
                 }
                 valStack.push(v);
-            }
-            else if(c == '+' || c == '-' || c == '*' || c == '/'){
+            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
                 opStack.push(c);
-            }
-            else return Double.NaN;
+            } else return Double.NaN;
         }
         return valStack.pop();
     }
