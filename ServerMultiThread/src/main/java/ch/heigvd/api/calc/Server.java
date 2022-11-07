64e4ea327a3bd5f50c6a3850bc8f21c40a780af2
Yann Merk <yann.merk@heig-vd.ch>
2022-11-07 09:36:26 +0100

    Implemented ServerMultiThread v1

--- ServerMultiThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerMultiThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -11,12 +11,14 @@ import java.util.logging.Logger;
  */
 public class Server {
 
+    private final int PORT = 4433;
+
     private final static Logger LOG = Logger.getLogger(Server.class.getName());
 
     /**
      * Main function to start the server
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // Log output on a single line
         System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
 
@@ -26,13 +28,28 @@ public class Server {
     /**
      * Start the server on a listening socket.
      */
-    private void start() {
+    private void start() throws IOException{
 
         /* TODO: implement the receptionist server here.
          *  The receptionist just creates a server socket and accepts new client connections.
          *  For a new client connection, the actual work is done in a new thread
          *  by a new ServerWorker.
          */
+
+        ServerSocket serverSocket = null;
+
+        // Create the server socket
+        serverSocket = new ServerSocket(PORT);
+
+        // Waiting for new clients to connect
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+
+            // Create new thread to handle this client
+            ServerWorker worker = new ServerWorker(clientSocket);
+            Thread thread = new Thread(worker);
+            thread.start();
+        }
 
     }
 }
